fca9ca8918be5f7162fdc5907f2e41c019638e54
Surya <surya.sk05@outlook.com>
2020-07-30 18:16:14 +0530

    cleaned up repeating code

--- src/screens/TA.php
+++ src/screens/TA.php
@@ -203,13 +203,6 @@
                   <a href="TAcourses.html">
                     <i class="metismenu-icon pe-7s-file"></i>
                     My Courses
-					  
-					 
-
-					
-										  
-														   
-							
                   </a>
                 </li>
               </ul>
@@ -235,37 +228,7 @@
         </div>
 		<?php	 
         session_start();
-          $courses=array();
-          $courses['CSCI 1800']=array(
-              "eName" => "John Doe", 
-              "CurrStudent" => "Yes", 
-              "Id" => "B00123456", 
-              "CourseID" => "CSCI 1800",
-              "Term" =>"Fall",
-              "year"=>"2021", 
-              "iName" => "Andrew X",
-              "CRN" => "1234" 
-          );
-          $courses['CSCI 2100']=array(
-              "eName" => "John Doe", 
-              "CurrStudent" => "Yes", 
-              "Id" => "B00123456", 
-              "CourseID" => "CSCI 2100",
-              "Term" =>"Winter",
-              "year"=>"2021", 
-              "iName" => "David Y",
-              "CRN" => "4561" 
-          );
-          $courses['CSCI 3000']=array(
-              "eName" => "John Doe", 
-              "CurrStudent" => "Yes", 
-              "Id" => "B00123456", 
-              "CourseID" => "CSCI 3000",
-              "Term" =>"Summer",
-              "year"=>"2022", 
-              "iName" => "Andrew Z",
-              "CRN" => "4563" 
-          );
+         include_once 'course-list.php';
           
         ?>		  
         <div class="app-main__outer">
