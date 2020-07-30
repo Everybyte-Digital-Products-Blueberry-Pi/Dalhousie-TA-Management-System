80ca2aa973135afbdc2cde6e2d452deed0930f70
Nhat Nguyen <nh635937@dal.ca>
2020-07-30 11:20:43 -0300

    Change status on pHourAprv

---
+++ src/scripts/scripts.pHourAprv.js
@@ -0,0 +1,5 @@
+function changeValue() {
+  var a = "Approved";
+  document.getElementById('status').innerHTML = a;
+  document.getElementById("status").className = "mb-2 mr-2 badge badge-success";
+}
