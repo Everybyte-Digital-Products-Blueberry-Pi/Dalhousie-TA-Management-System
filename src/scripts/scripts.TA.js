dc6dbb64f850e9f21355d077cb990ebfe0db2e4b
Nhat Nguyen <nh635937@dal.ca>
2020-08-06 09:31:46 -0300

    Revert changes on TA js

--- src/scripts/scripts.TA.js
+++ src/scripts/scripts.TA.js
@@ -47,10 +47,9 @@ function checkTotalHours(){
 new total hour the user put in to the sub-hours */
   if (e.options[e.selectedIndex].value == 0){
     checkTotal = document.getElementById('totalHours').value;
-    if (checkTotal < 0 || checkTotal > 130)){
-      alert('Hours should be between 0 and 130');
-    } else{
-        if (!(checkTotal == total)){
+        if (!(checkTotal > 0 &&
+              checkTotal < 130 &&
+              checkTotal == total)){
             alert('Total hours does not match, please check the value in the sub-hours section');
           } else{
             y.style.visibility = 'visible';
@@ -58,7 +57,6 @@ new total hour the user put in to the sub-hours */
             z.style.display = 'block';
           }
       }
-    }
 /*if the user choose one of the givenn option, calculate the
 sum of sub-hours to the total hours that the user choose*/
   if (e.options[e.selectedIndex].value != 0){
