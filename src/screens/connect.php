49f2c63737aee9a42d18e695bec977df65f2b79a
surya <surya.sk05@outlook.com>
2020-08-07 16:56:17 +0530

    add ability to change status

--- src/screens/connect.php
+++ src/screens/connect.php
@@ -8,7 +8,7 @@ $db_password = 'root';
 
 
 
-$conn = mysqli_connect($db_hostname, $db_username, $db_database, $db_database); //connecting to the database
+$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database); //connecting to the database
 
 if (!$conn) die("Unable to connect to MySQL: " . mysql_error()); //checking connection
 
