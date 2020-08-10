c7f74783aa3ad92ad90e6cb540b9e1524f7a45ac
mukaddim98 <mukaddim1998@gmail.com>
2020-08-10 13:13:20 -0300

    Changed the folder organisation

---
+++ assets/scripts/custom-scripts/scripts.pCourses.js
@@ -0,0 +1,13 @@
+//function is used to make the whole row clickable on modal
+  (function(){
+    ('#orderModal').modal({
+      keyboard: true,
+      backdrop: "static",
+      show:false,
+
+    }).on('show', function(){ //subscribe to show method
+      var getIdFromRow = $(event.target).closest('tr').data('id'); //get the id from tr
+      //make your ajax call populate items or what even you need
+      $(this).find('#orderDetails').html($('<b> Order Id selected: ' + getIdFromRow  + '</b>'))
+    });
+  });
